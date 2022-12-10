f338e4b62be6eabcb3e112784e0d877db62ae280
Pollytime <yoropolytime@gmail.com>
2022-12-09 19:04:05 -0800

    School is cool

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ int main(void)
 	num = n % 10;
 	if (num > 5)
 	{
-		printf("%d and is than 5\n", num);
+		printf("%d and is than 5\n", n, num);
 	}
 	else if ((num < 6) && (num < 0))
 	{
-		printf("%d and is less than 6 and not 0\n", num);
+		printf("%d and is less than 6 and not 0\n", n, num);
 	}
 	else
 	{
-		printf("%d and is 0\n", num);
+		printf("%d and is 0\n", n, num);
 	}
 	return (0);
 }
