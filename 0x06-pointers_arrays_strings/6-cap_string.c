c5b90507469c5d563741c731fab59c9fc6109307
Pollytime <yoropolytime@gmai.com>
2022-12-23 18:51:28 -0800

    Working

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,7 @@
  * cap_string - capitalizes every first letter of a word in a string
  * separators of words are: space, tabulation
  * new line
- * @s: Pointer to string
+ * @s: POinter to string
  *
  * Return: Pointer to s
  */
@@ -18,7 +18,11 @@ char *cap_string(char *s)
 		{
 			s[0] = s[0] - 32;
 		}
-		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n' || s[count] == ',' || s[count] ==';' || s[count] == '.' || s[count] == '.' || s[count] == '!' || s[count] == '?' || s[count] == '"' || s[count] == '(' || s[count] == ')' || s[count] == '{' || s[count] == '}')
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n' 
+		    || s[count] == ',' || s[count] ==';' || s[count] == '.' 
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?' 
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')' 
+		    || s[count] == '{' || s[count] == '}')
 		{
 			if (s[count + 1] >= 97 && s[count + 1] <= 122)
 			{
