76835edc5abc09f022654e72b86753b481587070
Pollytime <yoropolytime@gmai.com>
2022-12-23 19:03:48 -0800

    Please work

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,31 +8,22 @@
 char *leet(char *s)
 {
 	int count = 0, i;
-	int lower_case[] = {97, 
-101, 111, 116, 108};
-	int upper_case[] = {65, 69, 
-79, 84, 76};
-	int numbers[] = {52, 51, 
-48, 55, 49};
+	int lower_case[] = {97, 101, 111, 116, 108};
+	int upper_case[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
 
 
-	while (*(s + count) != 
-'\0')
+	while (*(s + count) != '\0')
 	{
-		for (i = 0; i < 5; 
-i++)
+		for (i = 0; i < 5; i++)
 		{
-			if (*(s + 
-count) == lower_case[i]) || *(s + 
-count) == upper_case[i]
+			if (*(s + count) == lower_case[i] || *(s + count) == upper_case[i])
 			{
-				*(s 
-+ count) = numbers[i];
-
-break;
+				*(s + count) = numbers[i];
+				break;
 			}
 		}
-		count++;
+		counter++;
 	}
 
 	return (s);
