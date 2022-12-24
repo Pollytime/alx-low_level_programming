d7a3450d2bb80e0e98e076b09d882595d76f021a
Pollytime <yoropolytime@gmai.com>
2022-12-23 17:08:42 -0800

    Four

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,12 +2,10 @@
 #include "main.h"
 
 /**
- * _strcat - function that 
- concatenates two strings.
+ * _strcat - function that concatenates two strings.
  * @dest: first string
  * @src: second string
- * Return: pointer to designated 
- string
+ * Return: pointer to designated string
  */
 
  char *_strcat(char *dest, char 
@@ -17,16 +15,13 @@
 
 	length = 0;
 
-	while (dest[length] != 
- '\0')
+	while (dest[length] != '\0')
 	{
 		length++;
 	}
-	for (j = 0; src[j] != '\0'; 
-  j++, length++)
+	for (j = 0; src[j] != '\0'; j++, length++)
   	 {
-		dest[length] = 
-  src[j];
+		dest[length] = src[j];
 	 } 
 
         dest[length] = '\0';
