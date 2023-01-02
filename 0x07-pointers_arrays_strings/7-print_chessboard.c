acb1b5489978c92a2f9f7ec20890ab76e8bbe33e
Pollytime <yoropolytime@gmai.com>
2023-01-02 15:52:59 -0800

    The fast

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,8 +11,8 @@ void print_chessboard(char (*a)[8])
 
 	for (row = 0; row < 8; row++)
 	{
-		for (column + 0; column < 8; column++)
-			_putchar(a[row][column]);
+		for (column = 0; column < 8; column++)
+		_putchar(a[row][column]);
 		_putchar('\n');
 	}
 }
